8328797aae6e291fec99c8deb4812faae2ee1df3
RyanDurling <rdurling@thoughtworks.com>
2012-08-23 23:50:32 -0700

    Can't get it to work yet.

--- ServerSocket/src/AcceptAClientConnection.java
+++ ServerSocket/src/AcceptAClientConnection.java
@@ -1,20 +1,22 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class AcceptAClientConnection {
     public static void main(String[] args) throws IOException {
         ServerSocket socket = null;
         try {
             socket = new ServerSocket(8080);
-            socket.setSoTimeout(1000000000);
+//            System.out.println("listening on port: " + socket.getLocalPort());
+//            Socket client = socket.accept();
+//            client.setKeepAlive(true);
+//            socket.setSoTimeout(1000000000);
         } catch (IOException e) {
             System.out.println("Could not listen on port: 8080");
             System.exit(-1);
         }
 
 //        socket;
-        Socket client = socket.accept();
+//        Socket client = socket.accept();
 
 //        client.connect();
 //        client
