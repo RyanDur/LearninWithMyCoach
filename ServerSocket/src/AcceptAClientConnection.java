959058109b8ec513b986350db1a54526ac303925
RyanDurling <rdurling@thoughtworks.com>
2012-08-23 23:54:03 -0700

    Working but no spinning. don't know if I'm doing it correctly.

--- ServerSocket/src/AcceptAClientConnection.java
+++ ServerSocket/src/AcceptAClientConnection.java
@@ -1,14 +1,15 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class AcceptAClientConnection {
     public static void main(String[] args) throws IOException {
         ServerSocket socket = null;
         try {
             socket = new ServerSocket(8080);
-//            System.out.println("listening on port: " + socket.getLocalPort());
-//            Socket client = socket.accept();
-//            client.setKeepAlive(true);
+            System.out.println("listening on port: " + socket.getLocalPort());
+            Socket client = socket.accept();
+            client.setKeepAlive(true);
 //            socket.setSoTimeout(1000000000);
         } catch (IOException e) {
             System.out.println("Could not listen on port: 8080");
