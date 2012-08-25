4bb5049e4d2813b1a94f7800017005bb19e7ea65
RyanDurling <ryan.p.durling@gmail.com>
2012-08-24 22:21:01 -0700

    first commit

--- ServerSocket/src/AcceptAClientConnection.java
+++ ServerSocket/src/AcceptAClientConnection.java
@@ -6,21 +6,16 @@ public class AcceptAClientConnection {
     public static void main(String[] args) throws IOException {
         ServerSocket socket = null;
         try {
-            socket = new ServerSocket(8080);
-            System.out.println("listening on port: " + socket.getLocalPort());
-            Socket client = socket.accept();
-            client.setKeepAlive(true);
-//            socket.setSoTimeout(1000000000);
+            while (true) {
+                socket = new ServerSocket(8080);
+                System.out.println("listening on port: " + socket.getLocalPort());
+                Socket client = socket.accept();
+                client.setKeepAlive(true);
+            }
         } catch (IOException e) {
             System.out.println("Could not listen on port: 8080");
             System.exit(-1);
         }
-
-//        socket;
-//        Socket client = socket.accept();
-
-//        client.connect();
-//        client
 
     }
 }
