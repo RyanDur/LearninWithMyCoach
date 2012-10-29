9267b2c5204abb185cac5cb01108ebb2b72054be
RyanDurling <ryan.p.durling@gmail.com>
2012-10-28 18:49:45 -0700

    Adding more thinking in Java Exercises

--- ThinkingInJavaFourthEdition/src/chapter/one/exercises/ExercisesOne.java
+++ ThinkingInJavaFourthEdition/src/chapter/one/exercises/ExercisesOne.java
@@ -23,7 +23,54 @@ public class ExercisesOne {
      *     </head>
      *     <body>
      *         <ol>
+     *             <li>Exercise 1:   (2) Create a class containing an int and a char that are not initialized, and
+     *                  print their values to verify that Java performs default initialization.</li>
      *
+     *             <li>Exercise 2:  (1) Following the HelloDate.java example in this chapter, create a hello, world
+     *                  program that simply displays that statement. You need only a single method in your class
+     *                  (the main one that gets executed when the program starts). Remember to make it static and to
+     *                  include the argument list, even though you do not use the argument list. Compile the program
+     *                  with javac and run it using java. If you are using a different development environment than
+     *                  the JDK, learn how to compile and run programs in that environment.</li>
+     *
+     *             <li>Exercise 3:  (1) Find the code fragments involving ATypeName and turn them into a program that
+     *                  compiles and runs.</li>
+     *
+     *             <li>Exercise 4:  (1) Turn the DataOnly code fragments into a program that compiles and runs.</li>
+     *
+     *             <li>Exercise 5:  (1) Modify the previous exercise so that the values of the data in DataOnly are
+     *                  assigned to and printed in main( ).</li>
+     *
+     *             <li>Exercise 6:  (2) Write a program that includes and calls the storage( ) method defined as a code
+     *                  fragment in this chapter.</li>
+     *
+     *             <li>Exercise 7:  (1) Turn the Incrementable code fragments into a working program.</li>
+     *
+     *             <li>Exercise 8:  (3) Write a program that demonstrates that, no matter how many objects you create
+     *                  of a particular class, there is only one instance of a particular static field in that
+     *                  class.</li>
+     *
+     *             <li>Exercise 9:  (2) Write a program that demonstrates that autoboxing works for all the primitive
+     *                  types and their wrappers.</li>
+     *
+     *             <li>Exercise 10:    (2) Write a program that prints three arguments taken from the command line. To
+     *                  do this, you will need to index into the command-line array of Strings.</li>
+     *
+     *             <li>Exercise 11:    (1) Turn the AllTheColorsOfTheRainbow example into a program that compiles and
+     *                  runs.</li>
+     *
+     *             <li>Exercise 13:    (1) Run Documentation1.java, Documentation2.java, and Documentation3.java
+     *                  through Javadoc. Verify the resulting documentation with your Web browser.</li>
+     *
+     *             <li>Exercise 14:    (1) Add an HTML list of items to the documentation in the previous exercise.</li>
+     *
+     *             <li>Exercise 15:    (1) Take the program in Exercise 2 and add comment documentation to it. Extract
+     *                  this comment documentation into an HTML file using Javadoc and view it with your Web
+     *                  browser.</li>
+     *
+     *             <li>Exercise 16:    (1) In the Initialization & Cleanup chapter, locate the Overloading.java example
+     *                  and add Javadoc documentation. Extract this comment documentation into an HTML file using
+     *                  Javadoc and view it with your Web  browser.</li>
      *         </ol>
      *     </body>
      * </html>
--- ThinkingInJavaFourthEdition/src/chapter/two/exercises/ExercisesTwo.java
+++ ThinkingInJavaFourthEdition/src/chapter/two/exercises/ExercisesTwo.java
@@ -15,7 +15,52 @@ public class ExercisesTwo {
      *     </head>
      *     <body>
      *         <ol>
+     *            <li>Exercise 1:   (1) Write a program that uses the short and normal form of print statement.</li>
      *
+     *            <li>Exercise 2:  (1) Create a class containing a float and use it to demonstrate aliasing.</li>
+     *
+     *            <li>Exercise 3:  (1) Create a class containing a float and use it to demonstrate aliasing during
+     *                  method calls.</li>
+     *
+     *            <li>Exercise 4:  (2) Write a program that calculates velocity using a constant distance and a
+     *                  constant time.</li>
+     *
+     *            <li>Exercise 5:  (2) Create a class called Dog containing two Strings: name and says. In main( ),
+     *                  create two dog objects with names spot (who says, Ruff!) and scruffy (who says, Wurf!). Then
+     *                  display their names and what they say.</li>
+     *
+     *            <li>Exercise 6:  (3) Following Exercise 5, create a new Dog reference and assign it to spots object.
+     *                  Test for comparison\ using == and equals( ) for all references.</li>
+     *
+     *            <li>Exercise 7:  (3) Write a program that simulates coin-flipping.</li>
+     *
+     *            <li>Exercise 8:  (2) Show that hex and octal notations work with long values. Use
+     *                  Long.toBinaryString( ) to display the results.</li>
+     *
+     *            <li>Exercise 9:  (1) Display the largest and smallest numbers for both float and double exponential
+     *                  notation.</li>
+     *
+     *            <li>Exercise 10:    (3) Write a program with two constant values, one with alternating binary ones
+     *                  and zeroes, with a zero in the least-significant digit, and the second, also alternating, with
+     *                  a one in the least-significant digit (hint: Its easiest to use hexadecimal constants for this).
+     *                  Take these two values and combine them in all possible ways using the bitwise operators, and
+     *                  display the results using Integer.toBinaryString( ).</li>
+     *
+     *            <li>Exercise 11:    (3) Start with a number that has a binary one in the most significant position
+     *                  (hint: Use a hexadecimal constant). Using the signed right-shift operator, right shift it all
+     *                  the way through all of its binary positions, each time displaying the result using
+     *                  Integer.toBinaryString( ).</li>
+     *
+     *            <li>Exercise 12:    (3) Start with a number that is all binary ones. Left shift it, then use the
+     *                  unsigned right-shift operator to right shift through all of its binary positions, each time
+     *                  displaying the result using Integer.toBinaryString( ).</li>
+     *
+     *            <li>Exercise 13:    (1) Write a method that displays char values in binary form. Demonstrate it using
+     *                  several different characters.</li>
+     *
+     *            <li>Exercise 14:    (3) Write a method that takes two String arguments and uses all the boolean
+     *                  comparisons to compare the two Strings and print the results. For the == and !=, also perform
+     *                  the equals( ) test. In main( ), call your method with some different String objects.</li>
      *         </ol>
      *     </body>
      * </html>
